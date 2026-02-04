754e8be72413a183c7d0dbb89027ca9eeac43fca
SalmaDorgham <2211146689@ku.edu.kw>
2026-02-04 22:39:04 +0300

    reset

--- backend/routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-const cartRouter = require("express").Router();
-const c = require("../controllers/cartController");
-const authUser = require("../middleware/auth");
-
-cartRouter.post("/get", authUser, c.getCart);
-cartRouter.post("/add", authUser, c.addtoCart);
-cartRouter.post("/update", authUser,  c.updateCart);
-
-module.exports = cartRouter;
