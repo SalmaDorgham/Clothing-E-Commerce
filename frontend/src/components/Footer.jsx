ac589456f336263b41731eee45c50899013c176a
SalmaDorgham <2211146689@ku.edu.kw>
2026-02-19 20:33:53 +0300

    Footer: fix error(vercel deployment)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
-import Form from "react-bootstrap/Form";
 import { Link } from 'react-router-dom'
 import { toast } from "react-toastify";
 
